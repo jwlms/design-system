4f4fb8aee5c329a87bda6a1b2cb7755d8fb36aac
Joshua Williams <josh@tucsonlabs.com>
2021-02-16 08:12:10 -0800

    Add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import babel from '@rollup/plugin-babel';
+import external from 'rollup-plugin-peer-deps-external';
+import del from 'rollup-plugin-delete';
+import pkg from './package.json';
+
+const config = {
+  input: pkg.source,
+  output: [
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'esm' }
+  ],
+  plugins: [
+    external(),
+    babel({
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled',
+    }),
+    del({ targets: ['dist/*'] }),
+  ],
+  external: Object.keys(pkg.peerDependencies || {}),
+};
+
+export default config;
